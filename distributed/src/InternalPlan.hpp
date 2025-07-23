b7686e646eb30ff1d6101bdd248d2c7637c55f7c
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2025-07-23 17:06:13 +0900

    Update Plan classes

---
+++ distributed/src/InternalPlan.hpp
@@ -0,0 +1,76 @@
+#ifndef INTERNAL_PLAN_HPP
+#define INTERNAL_PLAN_HPP
+
+#include <string>
+#include <Kokkos_Core.hpp>
+#include <KokkosFFT.hpp>
+
+template <typename ExecutionSpace, typename InViewType, typename OutViewType,
+          std::size_t DIM = 1>
+class InternalPlan {
+  using axes_type     = KokkosFFT::axis_type<DIM>;
+  using topology_type = KokkosFFT::shape_type<InViewType::rank()>;
+  using extents_type  = KokkosFFT::shape_type<InViewType::rank()>;
+
+  extents_type m_in_extents, m_out_extents;
+
+ public:
+  explicit InternalPlan(
+      const ExecutionSpace& exec_space, const InViewType& in,
+      const OutViewType& out, const axes_type& axes,
+      const topology_type& in_topology, const topology_type& out_topology,
+      const MPI_Comm& /*comm*/,
+      KokkosFFT::Normalization norm = KokkosFFT::Normalization::backward)
+      : m_in_extents(KokkosFFT::Impl::extract_extents(in)),
+        m_out_extents(KokkosFFT::Impl::extract_extents(out)) {}
+
+  virtual ~InternalPlan() = default;
+
+  /// \brief Forward FFT operation
+  /// \param[in] Input view
+  /// \param[out] Output view
+  virtual void forward(const InViewType& in, const OutViewType& out) const = 0;
+
+  /// \brief Backward FFT operation
+  /// \param[out] Output view
+  /// \param[in] Input view
+  virtual void backward(const OutViewType& out, const InViewType& in) const = 0;
+
+ protected:
+  void good(const InViewType& in, const OutViewType& out) const {
+    auto in_extents  = KokkosFFT::Impl::extract_extents(in);
+    auto out_extents = KokkosFFT::Impl::extract_extents(out);
+
+    auto mismatched_extents = [&](extents_type extents,
+                                  extents_type plan_extents) -> std::string {
+      std::string message;
+      message += "View (";
+      message += std::to_string(extents.at(0));
+      for (std::size_t r = 1; r < extents.size(); r++) {
+        message += ",";
+        message += std::to_string(extents.at(r));
+      }
+      message += "), ";
+      message += "Plan (";
+      message += std::to_string(plan_extents.at(0));
+      for (std::size_t r = 1; r < plan_extents.size(); r++) {
+        message += ",";
+        message += std::to_string(plan_extents.at(r));
+      }
+      message += ")";
+      return message;
+    };
+
+    KOKKOSFFT_THROW_IF(in_extents != m_in_extents,
+                       "extents of input View for plan and "
+                       "execution are not identical: " +
+                           mismatched_extents(in_extents, m_in_extents));
+
+    KOKKOSFFT_THROW_IF(out_extents != m_out_extents,
+                       "extents of output View for plan and "
+                       "execution are not identical: " +
+                           mismatched_extents(out_extents, m_out_extents));
+  }
+};
+
+#endif
