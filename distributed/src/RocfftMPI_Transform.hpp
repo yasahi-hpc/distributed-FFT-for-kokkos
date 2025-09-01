0e04414647d526dc7a5d7628b545ff329444aee5
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2025-09-02 02:32:42 +0900

    update rocfftMPI impl

--- distributed/src/RocfftMPI_Transform.hpp
+++ distributed/src/RocfftMPI_Transform.hpp
@@ -8,12 +8,12 @@
 template <typename ExecutionSpace, typename PlanType, typename InViewType,
           typename OutViewType>
 inline void exec_plan(
-    const ExecutionSpace& exec_space, const PlanType& scoped_plan,
+    const ExecutionSpace& /*exec_space*/, const PlanType& scoped_plan,
     const InViewType& in, const OutViewType& out,
-    const std::array<std::size_t, InViewType::rank()>& in_extents,
-    const std::array<std::size_t, OutViewType::rank()>& out_extents,
-    const KokkosFFT::axis_type<InViewType::rank()>& in_map,
-    const KokkosFFT::axis_type<OutViewType::rank()>& out_map,
+    const std::array<std::size_t, InViewType::rank()>& /*in_extents*/,
+    const std::array<std::size_t, OutViewType::rank()>& /*out_extents*/,
+    const KokkosFFT::axis_type<InViewType::rank()>& /*in_map*/,
+    const KokkosFFT::axis_type<OutViewType::rank()>& /*out_map*/,
     KokkosFFT::Direction direction) {
   Kokkos::Profiling::ScopedRegion region("exec_plan[TPL_RocfftMPIExec]");
   rocfft_status status =
