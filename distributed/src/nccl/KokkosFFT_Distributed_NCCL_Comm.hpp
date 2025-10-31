798d5de35016aece95642ae873276b4f6a2649d8
yasahi-hpc <y.asahi6412@gmail.com>
2025-10-31 19:09:24 +0900

    Introduce NCCL wrapper

---
+++ distributed/src/nccl/KokkosFFT_Distributed_NCCL_Comm.hpp
@@ -0,0 +1,43 @@
+#ifndef KOKKOSFFT_DISTRIBUTED_NCCL_Comm_HPP
+#define KOKKOSFFT_DISTRIBUTED_NCCL_Comm_HPP
+
+#include <cstdint>
+#include <mpi.h>
+#include <nccl.h>
+#include "KokkosFFT_Distributed_NCCL_Types.hpp"
+
+namespace KokkosFFT {
+namespace Distributed {
+namespace Impl {
+
+struct ScopedNCCLComm {
+  ncclComm_t m_comm;
+  int m_rank = 0;
+  int m_size = 1;
+
+  ScopedNCCLComm(const MPI_Comm &comm) {
+    ::MPI_Comm_rank(comm, &m_rank);
+    ::MPI_Comm_size(comm, &m_size);
+
+    ncclUniqueId id;
+    if (m_rank == 0) ncclGetUniqueId(&id);
+    ::MPI_Bcast(&id, sizeof(id), MPI_BYTE, 0, comm);
+    ::MPI_Barrier(comm);
+    ncclCommInitRank(&m_comm, m_size, id, m_rank);
+  }
+
+  ScopedNCCLComm() = delete;
+  ~ScopedNCCLComm() { ncclCommDestroy(m_comm); }
+
+  ncclComm_t comm() const { return m_comm; }
+  int rank() const { return m_rank; }
+  int size() const { return m_size; }
+};
+
+using TplComm = ScopedNCCLComm;
+
+}  // namespace Impl
+}  // namespace Distributed
+}  // namespace KokkosFFT
+
+#endif
---
+++ distributed/unit_test/nccl/Test_NCCL_Types.cpp
@@ -0,0 +1,46 @@
+#include <mpi.h>
+#include <sstream>
+#include <iostream>
+#include <gtest/gtest.h>
+#include <Kokkos_Core.hpp>
+#include "nccl/KokkosFFT_Distributed_NCCL_Types.hpp"
+
+namespace {
+using test_types = ::testing::Types<int, std::int32_t, std::uint32_t,
+                                    std::int64_t, std::uint64_t, float, double>;
+
+template <typename T>
+struct TestNCCLType : public ::testing::Test {
+  using value_type = T;
+};
+
+template <typename T>
+void test_nccl_data_type() {
+  ncclDataType_t nccl_data_type =
+      KokkosFFT::Distributed::Impl::NCCLDataType<T>::type();
+
+  if constexpr (std::is_same_v<T, int>) {
+    ASSERT_EQ(nccl_data_type, ncclInt);
+  } else if constexpr (std::is_same_v<T, std::int32_t>) {
+    ASSERT_EQ(nccl_data_type, ncclInt32);
+  } else if constexpr (std::is_same_v<T, std::uint32_t>) {
+    ASSERT_EQ(nccl_data_type, ncclUint32);
+  } else if constexpr (std::is_same_v<T, std::int64_t>) {
+    ASSERT_EQ(nccl_data_type, ncclInt64);
+  } else if constexpr (std::is_same_v<T, std::uint64_t>) {
+    ASSERT_EQ(nccl_data_type, ncclUint64);
+  } else if constexpr (std::is_same_v<T, float>) {
+    ASSERT_EQ(nccl_data_type, ncclFloat);
+  } else if constexpr (std::is_same_v<T, double>) {
+    ASSERT_EQ(nccl_data_type, ncclDouble);
+  }
+}
+
+}  // namespace
+
+TYPED_TEST_SUITE(TestNCCLType, test_types);
+
+TYPED_TEST(TestNCCLType, test_convert_scalar_type) {
+  using value_type = typename TestFixture::value_type;
+  test_nccl_data_type<value_type>();
+}
